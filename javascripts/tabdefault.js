9ebf21572963e614792c95f9759a6dbfe1339ed7
Mark Buttweiler <mark.buttweiler@gmail.com>
2017-09-05 22:05:14 -0400

    Fixed button issue

--- javascripts/tabdefault.js
+++ javascripts/tabdefault.js
@@ -1,8 +1,4 @@
 //This will choose the default tab
 
-<button class="tablinks" onclick="openCity(event, 'About Me')" id="defaultOpen">About Me</button>
-
-<script>
 // Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen").click();
-</script>
