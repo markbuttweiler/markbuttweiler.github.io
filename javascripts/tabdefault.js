1146b096fcf57ae2a2b45bd7dda208fae6ab3f7c
Mark Buttweiler <mark.buttweiler@gmail.com>
2017-09-05 21:57:27 -0400

    Updated CSS JS and HTML code for website

---
+++ javascripts/tabdefault.js
@@ -0,0 +1,8 @@
+//This will choose the default tab
+
+<button class="tablinks" onclick="openCity(event, 'About Me')" id="defaultOpen">About Me</button>
+
+<script>
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+</script>
